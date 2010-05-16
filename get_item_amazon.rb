a13acc77b498d1561b7e7379fb2859eb43a8595b
midore <midorex@gmail.com>
2010-05-16 13:06:44 +0900

    get_item_amazon
    このファイルの主目的
    必要なライブラリを require する。
    original file: lib/get_item

---
+++ get_item_amazon.rb
@@ -0,0 +1,19 @@
+
+# amazon_api.rb
+begin
+  raise "Error: only ruby 1.9.1" if RUBY_VERSION < "1.9.1"
+  ext = Encoding.default_external.name
+  raise "Error, LANG must be UTF-8" unless ext == 'UTF-8'
+end
+
+$:.unshift(File.dirname(__FILE__))
+
+require 'time'
+require 'timeout'
+require 'rexml/document'
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+require 'lib/get_item'
+
